3cb3a8c1418d1514dc2efe0f7d9ef9ee07abdf80
DalinOurn <ourndalin111@gmail.com>
2022-10-27 12:55:22 +0700

    same ip

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require("path");
 
 app.use(cors({
-    origin: 'http://18.181.83.28:3000',
+    origin: 'http://54.199.186.190:3000',
     credentials: true
 }))
 
@@ -42,4 +42,4 @@ app.use((err, req, res, next) => {
 })
 
 
-app.listen(process.env.PORT || 3001, () => console.log('App avaiable on http://18.183.54.118:3001'))
+app.listen(process.env.PORT || 3001, () => console.log('App avaiable on http://54.199.186.190:3001'))
