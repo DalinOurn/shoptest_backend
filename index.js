9bbc323db7186e0be2b00ac3b8be1a8db426ca80
DalinOurn <ourndalin111@gmail.com>
2022-11-05 12:57:38 +0700

    deploy image change

--- index.js
+++ index.js
@@ -42,4 +42,4 @@ app.use((err, req, res, next) => {
 })
 
 
-app.listen(process.env.PORT || 3001, () => console.log('App avaiable on http://3.112.52.2:3001'))
+app.listen(process.env.PORT || 3001, () => console.log('App avaiable on http://3.112.52.1:3001'))
